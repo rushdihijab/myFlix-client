2f5aeefefe35e5d39ac45aeb504c29f841660a59
rushdihijab <103061146+rushdihijab@users.noreply.github.com>
2022-10-26 15:13:01 -0400

    install parcel, Creating files (index.html, index.scss, index.jsx)
    use Parcel in myFlix project,

--- src/index.jsx
+++ src/index.jsx
@@ -9,7 +9,7 @@ class MyFlixApplication extends React.Component {
     render() {
         return (
             <div className="my-flix">
-                <div>Good morning</div>
+                <div>RUSHDI</div>
             </div>
         );
     }

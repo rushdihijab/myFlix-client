3bd197724bd693af448b4aaaf7e454ad85033584
rushdihijab <103061146+rushdihijab@users.noreply.github.com>
2022-10-27 10:24:57 -0400

    Update index.jsx

--- src/index.jsx
+++ src/index.jsx
@@ -9,7 +9,7 @@ class MyFlixApplication extends React.Component {
     render() {
         return (
             <div className="my-flix">
-                <div>RUSHDI</div>
+                <div>My Flix Movies APP</div>
             </div>
         );
     }

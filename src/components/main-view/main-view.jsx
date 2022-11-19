38fa3acd8b1527e810e80a9008f710f9a9bc836d
rushdihijab <103061146+rushdihijab@users.noreply.github.com>
2022-11-19 16:22:11 -0500

    Update main-view.jsx

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -1,37 +1,62 @@
 import React from 'react';
+import axios from 'axios';
+
 import { MovieCard } from '../movie-card/movie-card';
 import { MovieView } from '../movie-view/movie-view';
+import { LoginView } from '../login-view/login-view';
+import { RegistrationView } from '../registration-view/registration-view';
 
 export class MainView extends React.Component {
 
     constructor() {
         super();
         this.state = {
-            movies: [
-                { _id: 1, Title: 'Kill Bill: Vol. 1', Description: 'After awakening from a four-year coma, a former assassin wreaks vengeance on the team of assassins who betrayed her.', ImagePath: '...' },
-                { _id: 2, Title: 'The Shawshank Redemption', Description: 'desc2...', ImagePath: '...' },
-                { _id: 3, Title: 'Gladiator', Description: 'desc3...', ImagePath: '...' }
-            ]
-        }
+            movies: [],
+            selectedMovie: null,
+            user: null
+        };
     }
-    setSelectedMovie(newSelectedMovie) {
+    componentDidMount() {
+        axios.get('https://my-movies-rushdi.herokuapp.com/movies')
+            .then(Response => {
+                this.setState({
+                    movies: Response.data
+                });
+            })
+            .catch(error => {
+                console.log(error);
+            });
+    }
+
+    setSelectedMovie(movie) {
         this.setState({
-            selectedMovie: newSelectedMovie
+            selectedMovie: movie
         });
 
     }
+    /* When a user successfully logs in, this function updates the `user` property in state to that *particular user*/
 
+    onLoggedIn(user) {
+        this.setState({
+            user
+        });
+    }
+    onRegistration(newUser) {
+        this.setState({
+            newUser
+        });
+    }
 
     render() {
-        const { movies, selectedMovie } = this.state;
+        const { movies, selectedMovie, user, newUser } = this.state;
 
-
-        if (movies.length === 0) return <div className="main-view">The list is empty!</div>;
-
+        if (!user) return <LoginView onLoggedIn={user => this.onLoggedIn(user)} />;
+        if (!newUser) return <RegistrationView onRegistration={newUser => this.onRegistration(newUser)} />;
+        if (movies.length === 0) return <div className="main-view" />;
         return (
             <div className="main-view">
                 {selectedMovie
-                    ? <MovieView movie={selectedMovie} onBackClick={newSelectedMovie => { this.setSelectedMovie(newSelectedMovie); }} />
+                    ? <MovieView movie={selectedMovie} onBackClick={(newSelectedMovie) => { this.setSelectedMovie(newSelectedMovie); }} />
                     : movies.map(movie => (
                         <MovieCard key={movie._id} movie={movie} onMovieClick={(movie) => { this.setSelectedMovie(movie) }} />
                     ))
